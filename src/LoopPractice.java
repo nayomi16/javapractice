59ab4003845c8a08e7acbf6f6a7994c8b212cc71
nayomi16 <nayomirashmikaaa@gmail.com>
2020-10-25 13:01:46 +0530

    loop2 added

--- src/LoopPractice.java
+++ src/LoopPractice.java
@@ -9,7 +9,21 @@ public class LoopPractice {
             }
             for (int j=0;j<5-i;j++){
                 System.out.print("*");
+            }
+            System.out.print("\n");
+        }
 
+    }
+
+
+    public void loop2(){
+        for(int i=0;i<6;i++){
+
+            for(int j=1;j<=5-i;j++){
+                System.out.print(j);
+            }
+            for (int k=1;k<=i+1;k++){
+                System.out.print("&");
             }
             System.out.print("\n");
         }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         LoopPractice practice=new LoopPractice();
         practice.loop1();
+        System.out.print("\n\n");
+        practice.loop2();
+
 
     }
 }
